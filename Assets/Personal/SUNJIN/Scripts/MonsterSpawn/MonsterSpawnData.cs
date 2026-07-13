6013f987113b79c57e8278dfcfed39e96f82f78b
김선진 <rlatjswls14@gmail.com>
2026-07-13 14:55:23 +0900

    몬스터 스폰구현중

---
+++ Assets/Personal/SUNJIN/Scripts/MonsterSpawn/MonsterSpawnData.cs
@@ -0,0 +1,9 @@
+public class MonsterSpawnData
+{
+    public int Round { get; set; }
+    public string MonsterId { get; set; }
+    public string SpawnPointId { get; set; }
+    public int Count { get; set; }
+    public float StartDelay { get; set; }
+    public float SpawnInterval { get; set; }
+}
