042d90876f4f3333d6e863a6faeae3380ed54cf7
김유찬 <152022287+muchan918@users.noreply.github.com>
2026-09-03 22:40:16 +0900

    Fix: 디버그 패널 가드 추가

--- Assets/Scripts/Test/BalanceTestPanel.cs
+++ Assets/Scripts/Test/BalanceTestPanel.cs
@@ -40,6 +40,18 @@ public class BalanceTestPanel : MonoBehaviour
     // 낮에 미리 켜둔 무적이 나중에 생긴 본진에도 적용돼야 한다.
     bool _invincible;
 
+    private void Awake()
+    {
+#if !UNITY_EDITOR
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+
+        enabled = false;
+#endif
+    }
+
     private void Start()
     {
         // 본진이 "이미 있음"과 "앞으로 생김"을 한 경로로 처리한다(PlayerBase.cs의 씬 싱글톤 계약).
